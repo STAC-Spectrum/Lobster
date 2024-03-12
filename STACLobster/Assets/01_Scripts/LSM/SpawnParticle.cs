4edf083552e780762d0951244290e0d0f96e3317
bigumdo <showmethe0610@gmail.com>
2024-03-13 00:03:27 +0900

    AnimationMaterial

--- STACLobster/Assets/01_Scripts/LSM/SpawnParticle.cs
+++ STACLobster/Assets/01_Scripts/LSM/SpawnParticle.cs
@@ -6,12 +6,20 @@ using UnityEngine.VFX;
 public class SpawnParticle : MonoBehaviour
 {
     public VisualEffect visualEffect;
+    private Animator animator;
 
     // Update is called once per frame
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();    
+    }
+
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
+            animator.SetTrigger("On");
             visualEffect.Play();
         }
     }
