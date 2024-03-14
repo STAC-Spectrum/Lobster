47b30b7616845da6ff6a2be5a0388b1f01aad81c
Baetaekyung <tkwayi2129@gmail.com>
2024-03-14 20:04:51 +0900

    AI - 80퍼쯤 됌

---
+++ STACLobster/Assets/00_Scenes/TK/01.Scripts/Enemy/EnemyStates/EnemyIdleState.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyIdleState : EnemyState
+{
+    private float randomDirX;
+    private float randomDirZ;
+    private float currentTime;
+    private float lastChangeDirTime;
+
+    public EnemyIdleState(Enemy enemy, EnemyStateMachine stateMachine, string animName) : base(enemy, stateMachine, animName)
+    {
+    }
+
+    public override void Enter()
+    {
+        base.Enter();
+        ChangeDirection();
+    }
+
+    public override void Exit()
+    {
+        base.Exit();
+    }
+
+    public override void UpdateState()
+    {
+        base.UpdateState();
+
+        Move();
+
+        currentTime = Time.time;
+
+        if(currentTime > _enemy.changeDirTime + lastChangeDirTime)
+        {
+            lastChangeDirTime = Time.time;
+            ChangeDirection();
+        }
+    }
+
+    private void ChangeDirection()
+    {
+        randomDirX = Random.Range(Random.Range(-1f, -2.5f), Random.Range(1f, 2.5f));
+        randomDirZ = Random.Range(Random.Range(-1f, -2.5f), Random.Range(1f, 2.5f));
+
+        _enemy.randomDirection = new Vector2(randomDirX, randomDirZ);
+    }
+
+    private void Move()
+    {
+        _enemy.Rigid.velocity = new Vector3(
+            _enemy.randomDirection.x, 0, _enemy.randomDirection.y);
+    }
+}
