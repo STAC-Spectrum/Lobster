3a96c76c9fb569e29d42781dfc7d24b01024bb86
Mk <minrim0615@gmail.com>
2024-03-20 00:51:05 +0900

    스크립트 수정

--- STACLobster/Assets/01_Scripts/Mk/Player.cs
+++ STACLobster/Assets/01_Scripts/Mk/Player.cs
@@ -10,21 +10,17 @@ public class Player : Agent
     [SerializeField] private InputReader _inputReader;
     [SerializeField] private float _moveSpeed = 5f, _jumpPower = 7f, _forceGravity = 15f;
     [SerializeField] private LayerMask _layerMask;
-
     private PlayerInput _playerInput;
     private Rigidbody _rigidbody;
-    
     private Vector3 _velocity;
     private float _verticalVelocity;
-
     private Vector3 mousePos;
-    
+    private Camera _main;
+
     #region 수정 해야함 야발 프로토타입
 
     [SerializeField] private GameObject _lightTrail;
     [SerializeField] private GameObject _lightAnlge;
-
-    [SerializeField] private Camera _mouseCam;
 
     private bool _isMouse = false;
     private float _angle;
@@ -33,9 +29,10 @@ public class Player : Agent
 
     public Action MouseMoveEvent;
     private Vector2 _mousePos;
-    
+
     private void Awake()
     {
+        _main = Camera.main;
         _rigidbody = GetComponent<Rigidbody>();
         /*PlayerInput playerInput = new PlayerInput();
         playerInput.Player.Enable();*/
@@ -50,7 +47,7 @@ public class Player : Agent
     private void LightRushHandle(double pos)
     {
         //print($"마우스 위치 {_mouseCam.ScreenToWorldPoint(pos)}");
-        
+
         print(pos);
 
         /*if (_inputReader._playerActions.Player.BulletTime.WasReleasedThisFrame())
@@ -59,16 +56,16 @@ public class Player : Agent
             _mousePos = _mouseCam.ScreenToWorldPoint(pos);
             _rigidbody.AddForce(_mousePos, ForceMode.Impulse);
         }*/
-        
+
     }
 
     private void BulletTimeHandle(bool isPress)
     {
         if (isPress)
             Time.timeScale = 0.4f;
-        else if(isPress == false)
+        else if (isPress == false)
             Time.timeScale = 1f;
-        
+
     }
 
 
@@ -84,7 +81,7 @@ public class Player : Agent
 
     private void Update()
     {
-        
+
         // 테스트 큐브 바라보기
         // 방향 벡터 = 목표 벡터 - 시작 벡터
         // 이 함수 나중에 조져야 함
@@ -99,8 +96,8 @@ public class Player : Agent
         //마우스 조져 버려야함
         if (Mouse.current.rightButton.wasReleasedThisFrame)
         {
-            mousePos = _mouseCam.ScreenToWorldPoint(
-                new Vector3(Input.mousePosition.x, Input.mousePosition.y, _mouseCam.farClipPlane));
+            mousePos = Camera.main.ScreenToWorldPoint(
+                new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.farClipPlane));
             //MouseMoveEvent?.Invoke();
         }
 
@@ -112,29 +109,29 @@ public class Player : Agent
                 new Vector2(mousePos.x * 10, mousePos.y * 10).normalized * testpower, 
                 _mouseSpeed * Time.deltaTime);*/
         }
-        
+
     }
 
     private Vector3 MousePositionCalculate()
     {
-        Vector3 mousePos = _mouseCam.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         return new Vector3(mousePos.x, mousePos.y);
     }
 
     private void MouseMoveHandle()
     {
         _angle = Mathf.Atan2(mousePos.y - transform.position.y, mousePos.x - transform.position.x) * Mathf.Rad2Deg;
-        
-        
+
+
         //_lightAnlge.transform.LookAt(new Vector3(mousePos.x, _lightTrail.transform.rotation.y, 0));
-        _lightAnlge.transform.rotation = Quaternion.AngleAxis(_angle-90, Vector3.right);
-        
+        _lightAnlge.transform.rotation = Quaternion.AngleAxis(_angle - 90, Vector3.right);
+
         _lightAnlge.transform.rotation = Quaternion.Euler(_lightAnlge.transform.eulerAngles.x * -1, 90, 0);
-        
+
         _lightTrail.SetActive(true);
 
         _isMouse = true;
-        
+
         //_rigidbody.AddForce(new Vector3(mousePos.x * 10000, mousePos.y, 0).normalized * testpower, ForceMode.Impulse);
         StartCoroutine(MouseMoveStop());
     }
