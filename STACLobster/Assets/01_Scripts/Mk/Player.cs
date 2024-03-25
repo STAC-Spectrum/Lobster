67943ae04e3837eefba667aa6340cfb314b68067
Mk <minrim0615@gmail.com>
2024-03-26 00:45:57 +0900

    Player Scripts refactoring and variables correction

--- STACLobster/Assets/01_Scripts/Mk/Player.cs
+++ STACLobster/Assets/01_Scripts/Mk/Player.cs
@@ -8,62 +8,40 @@ using UnityEngine.InputSystem;
 public class Player : Agent
 {
     [SerializeField] private InputReader _inputReader;
-    [SerializeField] private float _moveSpeed = 5f, _jumpPower = 7f, _forceGravity = 15f;
-    [SerializeField] private LayerMask _layerMask;
-    [SerializeField] private float _maxDistance = 100f;
-    private PlayerInput _playerInput;
-    private Rigidbody _rigidbody;
-    private Vector3 _velocity;
-    private float _verticalVelocity;
-    private Vector3 mousePos;
     private Camera _main;
-    private bool _isJump = false;
 
-    #region 수정 해야함 야발 프로토타입
-
-    [SerializeField] private GameObject _lightTrail;
-    [SerializeField] private GameObject _lightAnlge;
-
-    [SerializeField] private float _lightPower = 8f;
-
-    private bool _isMouse = false;
-    private float _angle;
-
-    [SerializeField] private GameObject _mouseFollowObj;  // 마우스를 쫓아 다니는 오브젝트
-    // 주석을 생활화 하자 뭐가 뭔지 모르겠네
+    #region Move related variables
+    
+    [SerializeField] private float _moveSpeed = 5f, _jumpPower = 7f, _forceGravity = 15f;
+    
     #endregion
 
+    #region Rigidbody related variables
+
+    private Rigidbody _rigidbody;
+    private Vector3 _velocity;
+    
+    #endregion
+
+    #region Ray related variables
+
+    [SerializeField] private LayerMask _layerMask;
+    [SerializeField] private float _maxDistance = 100f;
+    
+    #endregion
+    
+    private Vector3 mousePos;  // 현재 gizmos 그리기 용
     public Action MouseMoveEvent;
-    private Vector2 _mousePos;
 
     private void Awake()
     {
         _main = Camera.main;
         _rigidbody = GetComponent<Rigidbody>();
-        //playerinput???
         PlayerInput playerInput = new PlayerInput();
         playerInput.Player.Enable();
         _inputReader.JumpEvent += JumpHandle;
         _inputReader.BulletTimeEvent += BulletTimeHandle;
-        _inputReader.LightRush += LightRushHandle;
         MouseMoveEvent += MouseMoveHandle;
-        //_lightTrail.SetActive(false);
-    }
-
-
-    private void LightRushHandle(double pos)
-    {
-        //print($"마우스 위치 {_mouseCam.ScreenToWorldPoint(pos)}");
-
-        print(pos);
-
-        /*if (_inputReader._playerActions.Player.BulletTime.WasReleasedThisFrame())
-        {
-            print(_mousePos);
-            _mousePos = _mouseCam.ScreenToWorldPoint(pos);
-            _rigidbody.AddForce(_mousePos, ForceMode.Impulse);
-        }*/
-
     }
 
     private void BulletTimeHandle(bool isPress)
@@ -74,8 +52,7 @@ public class Player : Agent
             Time.timeScale = 1f;
 
     }
-
-
+    
     public void SetPosition(Vector3 movement) //보류
     {
         _velocity = movement * Time.fixedDeltaTime;
@@ -84,50 +61,6 @@ public class Player : Agent
         {
             // rotation
         }
-    }
-
-    private void Update()
-    {
-        if (Mouse.current.rightButton.wasPressedThisFrame)
-        {
-            //print("마우스 눌림");
-            mousePos = Camera.main.ScreenToWorldPoint(
-                new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.farClipPlane));
-            //print(mousePos);
-            Vector3 moveDir = new Vector3(mousePos.x, mousePos.y, 0).normalized;
-            _rigidbody.AddForce(moveDir * _lightPower, ForceMode.Impulse);
-            if (Physics.Raycast(mousePos, Vector3.forward, out RaycastHit hit, _maxDistance, _layerMask))
-            {
-                print("맞음");
-                //여기 레이 맞으면 충돌 코드 추가해야함
-            }
-        }
-
-        // 테스트 큐브 바라보기
-        // 방향 벡터 = 목표 벡터 - 시작 벡터
-        // 이 함수 나중에 조져야 함
-        Vector3 targetPos = MousePositionCalculate();
-        Vector3 dir = targetPos - _lightAnlge.transform.position;
-        Quaternion rot = Quaternion.LookRotation(dir.normalized);
-
-        //마우스 따라다님
-        _mouseFollowObj.transform.position = GetMousePos();
-
-        _lightAnlge.transform.rotation = rot;
-        _lightAnlge.transform.eulerAngles = new Vector3(_lightAnlge.transform.eulerAngles.x + 180,
-            _lightAnlge.transform.eulerAngles.y, 0);
-
-        
-
-        if (_isMouse)
-        {
-            _lightTrail.SetActive(true);
-            /*transform.position = Vector2.MoveTowards(
-                transform.position,
-                new Vector2(mousePos.x * 10, mousePos.y * 10).normalized * testpower, 
-                _mouseSpeed * Time.deltaTime);*/
-        }
-
     }
 
     // 이 함수는 new input system이 대체 가능 
@@ -147,27 +80,7 @@ public class Player : Agent
 
     private void MouseMoveHandle()
     {
-        _angle = Mathf.Atan2(mousePos.y - transform.position.y, mousePos.x - transform.position.x) * Mathf.Rad2Deg;
-
-
-        //_lightAnlge.transform.LookAt(new Vector3(mousePos.x, _lightTrail.transform.rotation.y, 0));
-        _lightAnlge.transform.rotation = Quaternion.AngleAxis(_angle - 90, Vector3.right);
-
-        _lightAnlge.transform.rotation = Quaternion.Euler(_lightAnlge.transform.eulerAngles.x * -1, 90, 0);
-
-        _lightTrail.SetActive(true);
-
-        _isMouse = true;
-
-        //_rigidbody.AddForce(new Vector3(mousePos.x * 10000, mousePos.y, 0).normalized * testpower, ForceMode.Impulse);
-        StartCoroutine(MouseMoveStop());
-    }
-
-    private IEnumerator MouseMoveStop()
-    {
-        yield return new WaitForSeconds(0.32f);
-        _isMouse = false;
-        _lightTrail.SetActive(false);
+        
     }
 
     private void GravityCalculate()
@@ -179,20 +92,19 @@ public class Player : Agent
     private void FixedUpdate()
     {
         GravityCalculate();
-        //_playerInput.Player.Movement.ReadValue<Vector2>();
         Vector2 inputVector = _inputReader._playerActions.Player.Movement.ReadValue<Vector2>();
         _rigidbody.velocity = new Vector2(inputVector.x * _moveSpeed, _rigidbody.velocity.y);
     }
 
     public void JumpHandle()
     {
-
         if (Physics.Raycast(transform.position, Vector2.down, out RaycastHit hit, _maxDistance, _layerMask))
         {
             _rigidbody.velocity = new Vector3(_rigidbody.velocity.x, _jumpPower, _rigidbody.velocity.z);
         }
     }
 
+    // Gizmos
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.red;
