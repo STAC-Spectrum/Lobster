1a9e60700fb546e8a50eb4a8a263183088379106
bigumdo <showmethe0610@gmail.com>
2024-05-08 01:41:35 +0900

    TitleScene_2

---
+++ STACLobster/Assets/01_Scripts/LSM/LSM_UI/LSM_TitleBtnUI.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LSM_TitleBtnUI : MonoBehaviour
+{
+    
+    public void Btn()
+    {
+        
+    }
+
+}
