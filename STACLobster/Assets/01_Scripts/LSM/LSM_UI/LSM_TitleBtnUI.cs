f6abd33c42ba4b7d88424d2a3c99f421184480c6
bigumdo <showmethe0610@gmail.com>
2024-05-09 10:32:53 +0900

    Stage_1

--- STACLobster/Assets/01_Scripts/LSM/LSM_UI/LSM_TitleBtnUI.cs
+++ STACLobster/Assets/01_Scripts/LSM/LSM_UI/LSM_TitleBtnUI.cs
@@ -1,13 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LSM_TitleBtnUI : MonoBehaviour
 {
     
-    public void Btn()
+    public void OnGameStart()
     {
-        
+        SceneManager.LoadScene(0);
+    }
+
+    public void OnSetting()
+    {
+
+    }
+
+    public void OnExit()
+    {
+        UnityEditor.EditorApplication.isPlaying = false;
+        Application.Quit();
     }
 
 }
